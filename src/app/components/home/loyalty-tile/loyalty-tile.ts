0b1f278519ce025820623c2325fc8f4b7bd9919f
Urban Nomad <urbannomad381@gmail.com>
2026-07-14 20:39:58 +0200

    Created New Components

--- src/app/components/home/loyalty-tile/loyalty-tile.ts
+++ src/app/components/home/loyalty-tile/loyalty-tile.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-loyalty-tile',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './loyalty-tile.html',
-  styleUrl: './loyalty-tile.css',
+  styleUrl: './loyalty-tile.css'
 })
 export class LoyaltyTile {
-
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HomeBanner } from '../../components/home/home-banner/home-banner';
+import { LoyaltyTile } from '../../components/home/loyalty-tile/loyalty-tile';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HomeBanner, 
+    LoyaltyTile
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
