2220285eb570c8ac2ddba5c612389b2fee7fd4fb
Urban Nomad <urbannomad381@gmail.com>
2026-07-14 18:33:40 +0200

    Created home page and components

---
+++ src/app/components/home/home-banner/home-banner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-banner',
+  imports: [],
+  templateUrl: './home-banner.html',
+  styleUrl: './home-banner.css',
+})
+export class HomeBanner {
+
+}
